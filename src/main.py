c3b24a4e37e470350ba703f40d14c24516f3ec7e
Aidan Fitz <aidanbfitzgerald@gmail.com>
2021-08-24 01:18:56 +1200

    final implementation of model

--- src/main.py
+++ src/main.py
@@ -3,27 +3,26 @@
 # Imports!
 import os
 import random
-
+import time
 import cv2
 import numpy as np
-
-import tensorflow as tf
-import matplotlib.pyplot as plt
-
-import model
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.callbacks import TensorBoard
 
 # Config and Inits
+from HeartLabTest.src.model import cnn_xray
+
 data_dir = "chest_xray"
 size = (256, 256)
 
+
 # Fucntions!
 def img_2_arr(
-    img_path: str,
-    resize: bool = False,
-    grayscale: bool = True,
-    size: tuple = (256, 256),
+        img_path: str,
+        resize: bool = False,
+        grayscale: bool = True,
+        size: tuple = (256, 256),
 ) -> np.ndarray:
-
     """
     This function is responsible for opening an image, Preprocessing
     it by color or size and returning a numpy array.
@@ -67,8 +66,8 @@ def create_datasets(data_dir: str) -> np.ndarray:
     all_normal_img_paths = []
     all_viral_img_paths = []
     all_bact_img_paths = []
-    for cls in os.listdir(data_dir): # NORMAL or PNEUMONIA
-        for img in os.listdir(os.path.join(data_dir, cls)): # all images
+    for cls in os.listdir(data_dir):  # NORMAL or PNEUMONIA
+        for img in os.listdir(os.path.join(data_dir, cls)):  # all images
             if cls == "NORMAL":
                 all_normal_img_paths.append(os.path.join(data_dir, cls, img))
             elif "virus" in img:
@@ -78,31 +77,60 @@ def create_datasets(data_dir: str) -> np.ndarray:
 
     # 0 for normal, 1 for bacterial and 2 for viral
     dataset = (
-        [
-            [img_2_arr(path, grayscale=True, resize=True, size=size), 0]
-            for path in all_normal_img_paths
-        ]
-        + [
-            [img_2_arr(path, grayscale=True, resize=True, size=size), 1]
-            for path in all_bact_img_paths
-        ]
-        + [
-            [img_2_arr(path, grayscale=True, resize=True, size=size), 2]
-            for path in all_viral_img_paths
-        ]
+            [
+                [img_2_arr(path, grayscale=True, resize=True, size=size), 0]
+                for path in all_normal_img_paths
+            ]
+            + [
+                [img_2_arr(path, grayscale=True, resize=True, size=size), 1]
+                for path in all_bact_img_paths
+            ]
+            + [
+                [img_2_arr(path, grayscale=True, resize=True, size=size), 2]
+                for path in all_viral_img_paths
+            ]
     )
 
     return np.array(dataset, dtype="object")
 
 
-def main():
+def split_dataset(dataset):
+    # shuffle dataset for better training
+    random.shuffle(dataset)
+    X = []
+    y = []
+    # Split dataset into images and labels
+    for features, label in dataset:
+        X.append(features)
+        y.append(label)
+    X = np.array(X)
+    y = np.array(y)
+    return X, y
 
+
+def preprocess(X, y):
+    X = X.reshape(-1, size[0], size[1], 1)
+    # apply appropriate number of categories to y
+    y = to_categorical(y, 3)
+    # normalise the data
+    X = X / 255.0
+
+    return X, y
+
+
+
+def main():
     # get dataset
     dataset = create_datasets(data_dir)
+    model = cnn_xray()
+    X, y = split_dataset(dataset)
+    X, y = preprocess(X, y)
 
-    pass
+    NAME = "cnn_xray-{}".format(int(time.time()))
+    tensorboard = TensorBoard(log_dir='logs/{}'.format(NAME))
+
+    model.fit(X, y, batch_size=20, epochs=10, validation_split=0.1, callbacks=[tensorboard])
 
 
 if __name__ == "__main__":
-
     main()
--- src/model.py
+++ src/model.py
@@ -3,26 +3,29 @@
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Conv2D, MaxPool2D, Flatten
+from tensorflow.keras import regularizers
 
 
-
-def model_zero(loss='categorical_crossentropy', opt='adam'):
+def cnn_xray(loss='categorical_crossentropy', opt='adam'):
     """
     This function implements a small example model that you can use/modify.
     You are not limited to creating your own model, you can use already
     implemented models such as ResNets etc.
     """
+
     # building a linear stack of layers with the sequential model
     model = Sequential()
     # convolutional layer
-    model.add(Conv2D(25, kernel_size=(3,3), strides=(1,1), padding='valid', activation='relu', input_shape=(28,28,1)))
-    model.add(MaxPool2D(pool_size=(1,1)))
+    model.add(
+        Conv2D(64, kernel_size=(3, 3), strides=(1, 1), padding='valid', activation='relu', input_shape=(256, 256, 1), kernel_regularizer= regularizers.l2(0.001), bias_regularizer= regularizers.l2(0.001)))
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+    model.add(Conv2D(64, kernel_size=(3, 3), strides=(1, 1), padding='valid', activation='relu'))
+    model.add(MaxPool2D(pool_size=(2, 2)))
     # flatten output of conv
     model.add(Flatten())
-    # hidden layer
-    model.add(Dense(100, activation='relu'))
     # output layer
-    model.add(Dense(10, activation='softmax'))
+    model.add(Dense(3, activation='softmax'))
 
     # compiling the sequential model
     model.compile(loss=loss, metrics=['accuracy'], optimizer=opt)
